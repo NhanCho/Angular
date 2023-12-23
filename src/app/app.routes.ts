0af2c6cea6c518a44dcacdca3819993c907cbb29
NhanCho <itsmetremor1123@gmail.com>
2023-12-23 20:20:10 +0700

    push lan 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
-import { ProductComponent } from './components/product/product.component';
+// import { ProductComponent } from './components/product/product.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     {
-        path: 'products',
+        path: '',
         component: ProductListComponent
     },
     {
         path: 'products/:id',
         component: ProductDetailsComponent
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
     }
 ];
